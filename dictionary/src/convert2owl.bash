151f71942b31d96110192d625a33d37713e00873
fossilfriend <egreenfest@gmail.com>
2021-10-28 11:16:05 -0400

    mods to niagadsobo & owl rebuild

--- dictionary/src/convert2owl.bash
+++ dictionary/src/convert2owl.bash
@@ -1,3 +1,6 @@
 # convert niagads.obo to owl using robot https://github.com/ontodev/robot
 
 robot convert --input ../obo/niagads.obo --output ../owl/niagadsobo.owl -vvv
+
+# remove namespace niagadsobo# tag so that it will link terms when assembled into complete owl
+sed -i 's/niagadsobo#//' ../owl/niagadsobo.owl
